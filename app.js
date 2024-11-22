25b706ec37348be64bd5a009e9ca8f8e4806c019
NaveenWebDevloper <NaveenWebDev14@gmail.com>
2024-11-22 18:10:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector("#emoji");
+const emoji = [
+    "😊",
+    "😂",
+    "🤣",
+    "😍",
+    "🥰",
+    "🤩",
+    "🤗",
+    "🤔",
+    "🤫",
+    "🤭",
+];
+
+btn.addEventListener("mouseover", () => {
+    btn.innerHTML = emoji[Math.floor(Math.random() * emoji.length)];
+})
+  
